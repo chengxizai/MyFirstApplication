987075a2c5afb46c05399030a26a4c081e70041d
chengxizai <1652785891@qq.com>
2019-09-09 17:38:55 +0800

    设置资源字符串，通过名称获得资源id，在转化为字符串，调用setText

--- app/src/main/java/com/example/casper/MainActivity.java
+++ app/src/main/java/com/example/casper/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.casper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -15,5 +16,14 @@ public class MainActivity extends AppCompatActivity {
 
         textViewHelloWorld=findViewById(R.id.textViewHelloWorld);
         textViewHelloWorld.setText("你好，编程实现的hello world！");
+
+        //直接设置字符串资源ID
+        textViewHelloWorld.setText(this.getString(R.string.hello_world));
+
+        //通过资源名获得字符串
+        Context context=this.getApplicationContext();
+        String resName="hello_word";
+        int helloWorldId=context.getResources().getIdentifier(resName,"string", context.getPackageName());
+        textViewHelloWorld.setText(this.getString(helloWorldId));
     }
 }
